de84a22bfbf2bcee039a3cf60ccdf4461136f80d
spoyte <noe.de.91@gmail.com>
2024-03-10 16:14:19 +0000

    final contracts

--- packages/hardhat/contracts/ReceiverOnDispatch.sol
+++ packages/hardhat/contracts/ReceiverOnDispatch.sol
@@ -3,16 +3,25 @@
 
 // SPDX-License-Identifier: Ecosystem
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
-import "./ITeleporterMessenger.sol";
-import "./ITeleporterReceiver.sol";
+import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterMessenger.sol";
+import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterReceiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {TeleporterCCIPBnM} from "./tCCIP-BnM.sol";
 
-contract ReceiverOnDispatch is ITeleporterReceiver {
-
+contract ReceiverOnDispatch is ITeleporterReceiver, Ownable {
+    constructor(address initialOwner)
+        Ownable(initialOwner)
+    {}
     ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
 
     string public lastMessage;
+    address token_contract_address = address(0);
+
+    function set_token_contract_address (address _address) external onlyOwner{
+        token_contract_address = _address;
+    }
 
     function receiveTeleporterMessage(
         bytes32 originChainID,
@@ -21,7 +30,16 @@ contract ReceiverOnDispatch is ITeleporterReceiver {
     ) external {
         // Only the Teleporter receiver can deliver a message.
         require(msg.sender == address(teleporterMessenger), "ReceiverOnDispatch: unauthorized TeleporterMessenger");
-
+        // Decoding the function parameters
+        (
+            uint256 amount,
+            address recipient,
+            address token_address
+        ) = abi.decode(message, (uint256, address, address));
+        //call the token contract
+        //TeleporterCCIPBnM token_contract = TeleporterCCIPBnM(token_contract_address);
+        //trigger the teleporter
+        TeleporterCCIPBnM(token_contract_address).mint(recipient, amount);
         // Store the message.
         lastMessage = abi.decode(message, (string));
     }
--- packages/hardhat/contracts/SenderOnChain.sol
+++ packages/hardhat/contracts/SenderOnChain.sol
@@ -3,20 +3,26 @@
 
 // SPDX-License-Identifier: Ecosystem
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
-import "./ITeleporterMessenger.sol";
+import "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/ITeleporterMessenger.sol";
+import "@openzeppelin/contracts@5.0.2/access/AccessControl.sol";
 
-contract SenderOnCChain {
+contract SenderOnChain is AccessControl{
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
-
+    constructor(address defaultAdmin, address minter)
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
     /**
      * @dev Sends a message to another chain.
      */
     function sendMessage(
         address destinationAddress,
-        string calldata message
+        bytes calldata message
     ) external {
         teleporterMessenger.sendCrossChainMessage(
             TeleporterMessageInput({
@@ -28,8 +34,20 @@ contract SenderOnCChain {
                 }),
                 requiredGasLimit: 100000,
                 allowedRelayerAddresses: new address[](0),
-                message: abi.encode(message)
+                message: message
             })
         );
     }
+    /*
+     *destinationBlockchainID: 0x9f3be606497285d0ffbb5ac9ba24aa60346a9b1812479ed66cb329f394a4b1c7 
+     *destinationAddress: address of ReceiverOnDispatch
+     *calldata message: _amount and _to
+     *
+     *
+     */
+    // bytes message = abi.encode(
+    //     _amount,
+    //     _recipient,
+    //     _token
+    // );
 }
